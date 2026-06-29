d58be5f79218705d0d92db4ca7dd6636e6de75bb
Kashyapgaliya29 <kashyapgaliya295@gmail.com>
2026-06-29 17:37:25 +0530

    Fix Button for mobile

--- Frontend/src/components/TodoCard.jsx
+++ Frontend/src/components/TodoCard.jsx
@@ -23,23 +23,23 @@ const TodoCard = ({ todo,toggleStatus,deleteTodo,setEditedTodo }) => {
             <h1 className={`${todo.completed ? 'line-through text-zinc-600' : ''} text-2xl font-bold tracking-tight`}>{todo.title}</h1>
             <p className='font-stretch-50% text-lg text-gray-600 font-medium wrap-break-word pt-3'>{todo.description}</p>
             <p className='text-[15px] text-zinc-300 font-medium wrap-break-words pt-2.5 leading-relaxed'>📅 Due : {new Date(todo.duedate).toLocaleDateString('en-GB')}</p>
-            <div className='flex pt-5 lg:flex-row gap-2'>
+            <div className='flex pt-5 lg:flex-row gap-1.5'>
                 <button 
-                  className={`px-5 py-2 text-[13px] font-semibold rounded-2xl active:scale-95 ${todo.completed ? 'bg-emerald-500/10 border border-emerald-500/20 text-emerald-400':'bg-amber-500/10 border border-amber-500/20 text-amber-400'} text-white rounded `}
+                  className={`px-4 py-2 text-[10px] lg:px-5 lg:py-2 lg:text-[13px] font-semibold rounded-2xl active:scale-95 ${todo.completed ? 'bg-emerald-500/10 border border-emerald-500/20 text-emerald-400':'bg-amber-500/10 border border-amber-500/20 text-amber-400'} text-white rounded `}
                   onClick={()=>toggleStatus(todo.id)}>
                   {/* <span className='flex items-center justify-center gap-2'><img src={YesImage} alt="" className='h-5 w-5 object-contain opacity-80'/>{todo.completed ? 'Completed' : 'Pending'}</span> */}
                   <span className='flex items-center justify-center gap-2'><CircleCheckBig size={17} />{todo.completed ? 'Completed' : 'Pending'}</span>
                 </button>
 
                 <button 
-                    className='px-5 py-2 text-[13px] font-semibold rounded-2xl active:scale-95 bg-sky-500/10 border border-sky-500/20 text-sky-400'
+                    className='px-4 py-2 text-[10px] lg:px-5 lg:py-2 lg:text-[13px] font-semibold rounded-2xl active:scale-95 bg-sky-500/10 border border-sky-500/20 text-sky-400'
                     onClick={handleEdit}
                     // ><span className='flex items-center justify-center gap-2'><img src={EditImage} alt="" className='h-5 w-5'/>Edit</span>
                     ><span className='flex items-center justify-center gap-2'><Pencil size={17} />Edit</span>
                 </button>
 
                 <button 
-                  className='px-5 py-2 text-[13px] font-semibold rounded-2xl active:scale-95 bg-rose-500/10 border border-rose-500/20 text-rose-400' onClick={()=>deleteTodo(todo.id)}>
+                  className='px-4 py-2 text-[10px] lg:px-5 lg:py-2 lg:text-[13px]  font-semibold rounded-2xl active:scale-95 bg-rose-500/10 border border-rose-500/20 text-rose-400' onClick={()=>deleteTodo(todo.id)}>
                     {/* <span className='flex items-center justify-center gap-2'><img src={DeleteImage} alt="" className='h-5 w-5'/>Delete</span> */}
                     <span className='flex items-center justify-center gap-2'><Trash2 size={18} />Delete</span>
                 </button>
